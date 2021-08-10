3c7d7d5088f185d61d20ac33150ccb167693e8c1
Tyler Zimmerman <tzimmerman@apxstream.com>
2021-08-10 12:57:21 -0500

    INIT

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Row */}
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAu92oBM2YiSqPFCp2QsWuUtJ30A_Rs28g",
+    authDomain: "netflix-clone-9242e.firebaseapp.com",
+    projectId: "netflix-clone-9242e",
+    storageBucket: "netflix-clone-9242e.appspot.com",
+    messagingSenderId: "3866810503",
+    appId: "1:3866810503:web:827c8395c059e44b5c4022",
+    measurementId: "G-G37FXCVBCD"
+  };
